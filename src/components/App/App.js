1cddec0a96432d5a713b6d488bbb1f38524458c8
Paul Baik <paul.baik@maxar.com>
2020-11-06 09:48:19 -0500

    initial style components

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+
+import Alert from '../Alert/Alert';
+
+function App() {
+  return (
+     <div className="wrapper">
+       <Alert />
+     </div>
+  );
+}
+
+export default App;
