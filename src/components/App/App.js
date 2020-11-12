008c3140d411694c8ba4cf91e2af29683760595b
sukhyun.baik <paul.baik@maxar.com>
2020-11-12 15:32:43 -0500

    debugging tool

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,34 @@
+import React, {createContext, useState} from 'react'
 import './App.css';
-import Product from '../Product/Product';
+// import Product from '../Product/Product';
+import TextInfomation from '../TextInformation/TextInformation'
+
+export const TextContext = createContext();
+TextContext.displayName = 'TextContext';
 
 function App() {
-  return (
-    <div className="App">
-      <Product />
+  const [text, setText ] = useState('');
+
+  return(
+    <TextContext.Provider value={text}>
+      <div className="wrapper">
+     <label htmlFor="text">
+       Add Your Text Here:
+       <br />
+       <textarea
+         id="text"
+         name="text"
+         rows="10"
+         cols="100"
+         onChange={e => setText(e.target.value)}
+       >
+       </textarea>
+      </label>
+      <TextInfomation />
     </div>
-  );
+    </TextContext.Provider>
+    
+  )
 }
 
 export default App;
