dcd3cd2b77f3c2d8a985ea134699ffd4f8b787b2
sukhyun.baik <paul.baik@maxar.com>
2020-11-13 13:41:09 -0500

    event listener for react

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+// import logo from './logo.svg';
+import './App.css';
+import FileNamer from '../FileNamer/FileNamer'
+
+function App() {
+  return (
+    <FileNamer />
+  );
+}
+
+export default App;
